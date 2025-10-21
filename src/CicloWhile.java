f504c8967bad345e386c62836cb04dc51d71cddc
Diego de la Cruz Retuerto <U25319701@utp.edu.pe>
2025-10-21 06:05:00 -0500

    Clase ciclo while

---
+++ src/CicloWhile.java
@@ -0,0 +1,10 @@
+public class CicloWhile {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        var contador = 1;
+        while (contador <= 3) {
+            System.out.println(contador++);
+        }
+    }
+}
