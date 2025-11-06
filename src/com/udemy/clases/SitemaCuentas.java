4e0e1d1ca4d30622e2c7e9977685b687bc4a5a11
Diego de la Cruz Retuerto <U25319701@utp.edu.pe>
2025-11-06 06:01:45 -0500

    update

---
+++ src/com/udemy/clases/SitemaCuentas.java
@@ -0,0 +1,35 @@
+package com.udemy.clases;
+
+import java.util.Scanner;
+
+public class SitemaCuentas {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        var salir = false;
+
+
+        while (!salir) {
+            System.out.print("""
+                    Sistema de administración de cuentas
+                    Menu:
+                    1. Crear cuenta
+                    2. Eliminar cuenta
+                    3. Salir
+                    Escoge una opción:\s""");
+
+            var option = sc.nextInt();
+
+            switch (option) {
+                case 1 -> System.out.println("Creación de cuenta");
+                case 2 -> System.out.println("Eliminar cuenta");
+                case 3 -> {
+                    System.out.println("Saliendo del sistema...");
+                    salir = true;
+                }
+                default -> System.out.println("Seleccione una opción valida");
+            }
+
+        }
+    }
+}
