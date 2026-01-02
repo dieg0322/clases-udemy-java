088a2afd2a60bb32fc7639fb882cd4b94b9c4904
Diego de la Cruz Retuerto <U25319701@utp.edu.pe>
2026-01-01 20:40:30 -0500

    Update

--- src/com/udemy/clases/arreglos/Arreglos.java
+++ src/com/udemy/clases/arreglos/Arreglos.java
@@ -1,15 +1,21 @@
 package com.udemy.clases.arreglos;
 
+import java.util.Scanner;
+
 public class Arreglos {
     public static void main(String[] args) {
-        var enteros = new int[5];
 
-        enteros[0] = 1;
-        enteros[4] = 22;
-        System.out.println("Valor 0: " + enteros[0]);
-        System.out.println("Valor 1: " + enteros[1]);
-        System.out.println("Valor 2: " + enteros[2]);
-        System.out.println("Valor 3: " + enteros[3]);
-        System.out.println("Valor 4: " + enteros[4]);
+        var consola = new Scanner(System.in);
+        System.out.println("Proporciona el tamaño del arreglo:");
+        var largoArreglo = Integer.parseInt(consola.nextLine());
+        var enteros = new int[largoArreglo];
+        for (int i = 0; i < largoArreglo; i++) {
+            System.out.println("Elemento en el índice " + i + ": " );
+            enteros[i] = Integer.parseInt(consola.nextLine());
+        }
+        System.out.println("Los elementos del arreglo son:");
+        for (int i = 0; i < largoArreglo; i++) {
+            System.out.println("Elemento en el índice " + i + ": " + enteros[i]);
+        }
     }
 }
